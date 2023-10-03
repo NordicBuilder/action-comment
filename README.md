ca7d97273ecaf638e0b35ba00a0edb055b18c252
Sebastian Wezel <sebastian.wezel@nordicsemi.no>
2023-10-03 13:39:49 +0200

    converting to a more generic comment action
    Signed-off-by: Sebastian Wezel <sebastian.wezel@nordicsemi.no>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# action-contribs
+# action-comment
+
+Post or update a comment on a pull request.
+In an update case it will check if a given reaction is present by the pull request author
--- action.py
+++ action.py
@@ -7,8 +7,8 @@ import os
 import sys
 
 NOTE = "\n\n*Note: This comment is automatically posted and updated by the " \
-       "Contribs GitHub Action.* "
- 
+       "Comment GitHub Action.* "
+
 def gh_tuple_split(s):
     sl = s.split('/')
     if len(sl) != 2:
@@ -21,25 +21,27 @@ def main():
     parser = argparse.ArgumentParser(
         description="GH Action script for contribution management",
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-c', '--command', action='store',
-                        choices=['external'],
-                        required=True,
-                        help='Command to execute.')
+    parser.add_argument('-n', '--note', action='store',
+                        required=False,
+                        help='Note string.')
 
-    parser.add_argument('-m', '--messages', action='store',
+    parser.add_argument('-m', '--message', action='store',
                         required=False,
                         help='Messages to post.')
 
-    parser.add_argument('-l', '--labels', action='store',
+    parser.add_argument('-r', '--reaction', action='store',
                         required=False,
-                        help='Comma-separated list of labels.')
+                        help='Reaction to check for.')
 
     print(sys.argv)
 
     args = parser.parse_args()
 
-    messages = [x.strip() for x in args.messages.split('|')]
-    labels = [x.strip() for x in args.labels.split(',')]
+    note = NOTE
+    if args.note:
+        note = args.note
+    if not note.startswith("\n\n"):
+        note = "\n\n" + note
 
     # Retrieve main env vars
     action = os.environ.get('GITHUB_ACTION', None)
@@ -47,13 +49,13 @@ def main():
     org_repo = os.environ.get('GITHUB_REPOSITORY', None)
 
     print(f'Running action {action} from workflow {workflow} in {org_repo}')
-    
+
     evt_name = os.environ.get('GITHUB_EVENT_NAME', None)
     evt_path = os.environ.get('GITHUB_EVENT_PATH', None)
     workspace = os.environ.get('GITHUB_WORKSPACE', None)
 
     print(f'Event {evt_name} in {evt_path} and workspace {workspace}')
- 
+
     token = os.environ.get('GITHUB_TOKEN', None)
     if not token:
         sys.exit('Github token not set in environment, please set the '
@@ -77,42 +79,33 @@ def main():
 
     print(f'org: {org} repo: {repo}')
 
-    gh_org = gh.get_organization(org)
-    gh_usr = gh.get_user(login)
-    member = gh_org.has_in_members(gh_usr)
-    nstr = '' if member else 'NOT '
-
-    print(f'User {login} is {nstr}a member of org {org}')
-
     tk_usr = gh.get_user()
     gh_repo = gh.get_repo(org_repo)
     gh_pr = gh_repo.get_pull(int(pr['number']))
 
     comment = None
     for c in gh_pr.get_issue_comments():
-        if c.user.login == tk_usr.login and NOTE in c.body:
+        if c.user.login == tk_usr.login and note in c.body:
             comment = c
             break
 
-    message = messages[0] + NOTE
-    if not comment and not member:
+    message = args.message + note
+    found = False
+    if not comment:
         print('Creating comment')
         gh_pr.create_issue_comment(message)
-    elif comment and member and len(messages) > 1:
-        print('Updating comment')
-        comment.edit(messages[1] + NOTE)
-
-    if not member:
-        print('Adding labels')
-        for l in labels:
-            gh_pr.add_to_labels(l)
     else:
-        print('Removing labels')
-        try:
-            for l in labels:
-                gh_pr.remove_from_labels(l)
-        except GithubException as e:
-            print('Unable to remove labels')
+        print('Updating comment')
+        comment.edit(message)
+        if args.reaction:
+            for r in comment.get_reactions():
+                print(r.content)
+                if r.user == gh_pr.user and r.content == args.reaction:
+                    print(f"Found reaction {args.reaction} by pr-user in comment")
+                    found = True
+
+    with open(os.getenv('GITHUB_OUTPUT'), 'a') as fh:
+        print(f"found_reaction={found}", file=fh)
 
     sys.exit(0)
 
